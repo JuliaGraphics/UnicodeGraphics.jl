b1adbf1b0c13e50c0c8fba5e02db87a9f8b9f8ea
Rafael Schouten <rafaelschouten@gmail.com>
2020-07-29 01:01:48 +1000

    cleanup docs

--- src/UnicodeGraphics.jl
+++ src/UnicodeGraphics.jl
@@ -7,6 +7,7 @@ export blockize, brailize, blockize!, brailize!
 
 """
     brailize(a, cutoff=0)
+
 Convert an array to a block unicode string, filling values above the cutoff point.
 """
 blockize(a, cutoff=0) = blockize!(initblock(size(a)), a, cutoff) 
@@ -17,13 +18,9 @@ initblock(y, x) = Array{Char,2}(undef, x + 1, (y - 1) ÷ 2 + 1)
 
 """
     blockize!(out, a, cutoff=0)
+
 Convert an array to a braile unicode string, filling the `out` array.
 Calculation of array dims is a little complicated:
-
-```julia
-yrange, xrange = axes(a)
-out = Array{Char,2}(undef, length(xrange) + 1, (length(yrange) - 1) ÷ 2 + 1)
-```
 """
 blockize!(out, a, cutoff=0) = join(block_array!(out, a, cutoff))
 
@@ -52,6 +49,7 @@ const braile_hex = ((0x01, 0x08), (0x02, 0x10), (0x04, 0x20), (0x40, 0x80))
 
 """
     brailize(a, cutoff=0)
+
 Convert an array to a braile unicode string, filling values above the cutoff point.
 """
 brailize(a, cutoff=0) = brailize!(initbraile(size(a)), a, cutoff) 
@@ -62,13 +60,8 @@ initbraile(y, x) = Array{Char,2}(undef, (x - 1) ÷ 2 + 2, (y - 1) ÷ 4 + 1)
 
 """
     brailize!(out, a, cutoff=0)
-Convert an array to a braile unicode string, filling the `out` array.
-Calculation of array dims is a little complicated:
 
-```julia
-yrange, xrange = axes(a)
-out = Array{Char,2}(undef, (length(xrange) - 1) ÷ 2 + 2, (length(yrange) - 1) ÷ 4 + 1)
-```
+Convert an array to a braile unicode string, filling the `out` array.
 """
 brailize!(out, a, cutoff=0) = join(braile_array!(out, a, cutoff))
 
